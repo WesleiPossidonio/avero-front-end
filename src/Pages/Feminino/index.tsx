8e4609e53efa5780fd4feec80714e9b884d7e17a
Weslei Possidonio <weslei.possidonio@gmail.com>
2025-12-02 00:03:30 -0300

    uupdate

--- src/Pages/Feminino/index.tsx
+++ src/Pages/Feminino/index.tsx
@@ -2,11 +2,12 @@
 import { CallAction } from "../../components";
 import { Aside, HeroProducts } from "./Components";
 import BackgroundImage from "../../assets/feminino.jpg";
+import MobileImage from '@/assets/MobileFeminino.png'
 
 export const FemininoSection = () => {
   return (
     <section className="w-full">
-      <img className="w-full" src={BackgroundImage} alt="" />
+      <img className="w-full" src={BackgroundImage} srcSet={MobileImage} alt="" />
 
       <div className="max-w-[80%] mx-auto min-h-screen flex gap-10 px-6 md:px-12 py-10">
         <Aside />
--- src/Pages/Masculino/index.tsx
+++ src/Pages/Masculino/index.tsx
@@ -3,11 +3,12 @@ import { CallAction } from "../../components";
 import { Aside, HeroProducts } from "./Components";
 import BackgroundImage from "../../assets/Masculino.png";
 import { StoreMap } from "../Home/Components";
+import MobileImage from '@/assets/MobileFeminino.png'
 
 export const MasculinoSection = () => {
   return (
     <section className="w-full">
-      <img className="w-full" src={BackgroundImage} alt="" />
+      <img className="w-full" src={BackgroundImage} srcSet={MobileImage} alt="" />
 
       <div className="lg:max-w-[80%] mx-auto min-h-screen flex gap-10 px-6 md:px-12 py-10">
         <Aside />
